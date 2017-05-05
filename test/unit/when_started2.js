1e1df76e5801c2db31405f8ec0c536985abaa18f
cheekytinker <github@cheekytinker.com>
2017-05-06 00:33:17 +0100

    Try ot see why cover is only running one test file on circle but ok locally

---
+++ test/unit/when_started2.js
@@ -0,0 +1,14 @@
+import { describe, it } from 'mocha';
+import start from '../../src/app';
+
+describe('unit', () => {
+  describe('account', () => {
+    describe('app', () => {
+      describe('when started2', () => {
+        it('should...', () => {
+          start();
+        });
+      });
+    });
+  });
+});
