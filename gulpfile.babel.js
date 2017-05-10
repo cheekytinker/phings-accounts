21be85fc2f4ca1d26ddb19b5514297a6efc7447e
cheekytinker <github@cheekytinker.com>
2017-05-10 16:22:48 +0100

    Try to get coverage of all tests

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,9 @@ const plumber = require('gulp-plumber');
 const mocha = require('gulp-mocha');
 const clean = require('gulp-clean');
 const istanbul = require('gulp-istanbul');
+const babelistanbul = require('gulp-babel-istanbul');
+const injectModules = require('gulp-inject-modules');
+const isparta = require('isparta');
 
 const testFiles = 'test/**/*.js';
 const srcFiles = 'src/**/*.js';
@@ -33,5 +36,26 @@ gulp.task('runtests', ['transpileSource'], () => {
     .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest('./build/test/'))
     .pipe(mocha());
+});
+
+gulp.task('coverage', function (cb) {
+  gulp.src('src/**/*.js')
+    .pipe(babelistanbul({presets: ['es2015']}))
+    .pipe(gulp.dest('./build/src/'))
+
+    //.pipe(babelistanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src('test/**/*.js')
+        .pipe(injectModules())
+        .pipe(babelistanbul({presets: ['es2015']}))
+        .pipe(gulp.dest('./build/test/'))
+        .pipe(mocha())
+        .pipe(babelistanbul({
+          instrumenter: isparta.Instrumenter,
+        }))
+        .pipe(babelistanbul.writeReports())
+        .pipe(babelistanbul.enforceThresholds({ thresholds: { global: 90 } }))
+        .on('end', cb);
+    });
 });
 
