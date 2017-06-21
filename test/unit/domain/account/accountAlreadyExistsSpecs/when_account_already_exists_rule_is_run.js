d0142b25c6df62beb94b8e7654eadaf9ea667812
cheekytinker <github@cheekytinker.com>
2017-06-21 09:28:17 +0100

    Business rule tests passing

---
+++ test/unit/domain/account/accountAlreadyExistsSpecs/when_account_already_exists_rule_is_run.js
@@ -0,0 +1,66 @@
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import chai from 'chai';
+import dirtyChai from 'dirty-chai';
+import sinon from 'sinon';
+import * as rule from '../../../../../src/domain/account/accountAlreadyExists';
+
+const expect = chai.expect;
+chai.use(dirtyChai);
+
+describe('unit', () => {
+  describe('domain', () => {
+    describe('account', () => {
+      describe('when account already exists rule is run', () => {
+        const changed = {};
+        const events = {};
+        const previous = {
+          get: () => {},
+        };
+        const command = {
+          name: 'startAccountSignup',
+        };
+        let stubGet = null;
+        beforeEach(() => {
+          stubGet = sinon.stub(previous, 'get');
+          stubGet.withArgs('status').returns(undefined);
+        });
+        afterEach(() => {
+          stubGet.restore();
+          command.name = 'startAccountSignup';
+        });
+        it('should be named accountAlreadyExists', () => {
+          expect(rule.name).is.equal('accountAlreadyExists');
+        });
+        it('should not raise an error if status is undefined', () => {
+          expect(rule.businessRuleFn.bind(
+            rule,
+            changed,
+            previous,
+            events,
+            command)).not.to.throw();
+        });
+        it('should not raise an error if a different command', () => {
+          stubGet.withArgs('status').returns('created');
+          command.name = 'anotherCommand';
+          expect(rule.businessRuleFn.bind(
+            rule,
+            changed,
+            previous,
+            events,
+            command)).not.to.throw();
+        });
+        it('should raise error if command is startAccountSignup and account already exists', () => {
+          stubGet.restore();
+          stubGet = sinon.stub(previous, 'get');
+          stubGet.withArgs('status').returns('created');
+          expect(rule.businessRuleFn.bind(
+            rule,
+            changed,
+            previous,
+            events,
+            command)).to.throw();
+        });
+      });
+    });
+  });
+});
