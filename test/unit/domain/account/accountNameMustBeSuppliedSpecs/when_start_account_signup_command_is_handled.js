5e60b3c3d37b81168a467a17eba139af2886e59a
cheekytinker <github@cheekytinker.com>
2017-06-21 15:42:54 +0100

    Precondition unit test added

---
+++ test/unit/domain/account/accountNameMustBeSuppliedSpecs/when_start_account_signup_command_is_handled.js
@@ -0,0 +1,36 @@
+import { describe, it } from 'mocha';
+import sinon from 'sinon';
+import chai from 'chai';
+import dirtyChai from 'dirty-chai';
+import * as precon from '../../../../../src/domain/account/accountNameMustBeSupplied';
+
+const expect = chai.expect;
+chai.use(dirtyChai);
+
+describe('unit', () => {
+  describe('domain', () => {
+    describe('account', () => {
+      describe('when start account signup command is handled', () => {
+        it('should be named after the command it handles', () => {
+          expect(precon.name).is.equal('startAccountSignup');
+        });
+        it('should error when name is less than 2 characters', () => {
+          const data = {
+            name: 'a',
+          };
+          const callback = sinon.spy();
+          precon.preLoadConditionFn(data, callback);
+          expect(callback.calledWith('Account name of more than 1 character must be supplied')).is.true();
+        });
+        it('should succeed when name greater than 1 character', () => {
+          const data = {
+            name: 'ab',
+          };
+          const callback = sinon.spy();
+          precon.preLoadConditionFn(data, callback);
+          expect(callback.calledWith(null)).is.true();
+        });
+      });
+    });
+  });
+});
