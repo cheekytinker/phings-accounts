ecbb1d24d38a1a6ac4a42e24cf020bf6cf6bb11b
cheekytinker <github@cheekytinker.com>
2017-06-21 22:14:12 +0100

    Command unit test added

---
+++ test/unit/domain/account/startAccountSignupSpecs/when_command_executed.js
@@ -0,0 +1,45 @@
+import { describe, it } from 'mocha';
+import chai from 'chai';
+import dirtyChai from 'dirty-chai';
+import sinon from 'sinon';
+import * as cmd from '../../../../../src/domain/account/startAccountSignup';
+
+const expect = chai.expect;
+chai.use(dirtyChai);
+
+describe('unit', () => {
+  describe('domain', () => {
+    describe('account', () => {
+      describe('when command executed', () => {
+        it('should be named "startAccountSignup"', () => {
+          expect(cmd.name).to.equal('startAccountSignup');
+        });
+        it('should set status to be "created"', () => {
+          const data = {
+            name: 'myName',
+          };
+          const agg = {
+            apply: () => {},
+          };
+          cmd.cmdFn(data, agg);
+          expect(data.status).to.equal('created');
+        });
+        it('should apply "accountSignupStarted" event to aggregate with data', () => {
+          const data = {
+            name: 'myName',
+          };
+          const agg = {
+            apply: () => {},
+          };
+          const expectedEventDataToApply = data;
+          expectedEventDataToApply.status = 'created';
+          const mockAgg = sinon.mock(agg);
+          const nameOfEventToApply = 'accountSignupStarted';
+          mockAgg.expects('apply').withArgs(nameOfEventToApply, expectedEventDataToApply);
+          cmd.cmdFn(data, mockAgg.object);
+          mockAgg.verify();
+        });
+      });
+    });
+  });
+});
